bb600716e376368ef458b9f323d993ec147f307f
Kristoffer Vollebæk <krivoll@stud.ntnu.no>
2025-06-16 00:46:30 +0200

    Skytinggit add . Kinda ;)

--- Assets/Scripts/Movement stuff/BulletMovement.cs
+++ Assets/Scripts/Movement stuff/BulletMovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,23 +7,26 @@ public class BulletMovement : MonoBehaviour
 {
     public GameObject player;
     public Vector2 position;
-    public InputActionReference move;
-    public InputActionReference direction;
-    public Vector2 speed;
+    public float bulletSpeed = 10f;
     public Rigidbody2D rb;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        position = move.action.ReadValue<Vector2>();
+        position = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // rb.linearVelocity = new Vector2(retning.x * fart, retning.y * fart);
 
-        transform.position = player.transform.position;
+        //transform.position = player.transform.position;
+        float posx, posy;
+        if (position.x > position.y) { posy = 0; posx = position.x; }
+        else { posx = 0; posy = position.y; }
+
+        rb.linearVelocity = new Vector2(posx * bulletSpeed, posy*bulletSpeed);
     }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,23 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Shooting : MonoBehaviour
 {
     public float bulletSpeed;
 
-    public GameObject bullet; 
-    
+    public GameObject bullet;
+    public InputActionReference skyt;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetButtonDown("Space")) {
-            Instantiate(bullet);
-        }
-        
+
+    }
+    void OnEnable()
+    {
+        skyt.action.started += Fire;
+    }
+    void OnDisable()
+    {
+        skyt.action.started -= Fire;
+    }
+    private void Fire(InputAction.CallbackContext obj)
+    {
+        Instantiate(bullet, transform.position, Quaternion.identity);
     }
 }
